2d3495057819f391ba062e10d6498e1b1405f107
zackthomas1 <zackthomas13@gmail.com>
2022-10-25 16:02:06 -0700

    Fixed big in OpenGLVertexArray
    AddVertexBuffer not adding multiple VertexBuffers correctly.

--- Hazzel/src/Platform/OpenGl/OpenGLVertexArray.h
+++ Hazzel/src/Platform/OpenGl/OpenGLVertexArray.h
@@ -19,6 +19,7 @@ namespace Hazzel
 		virtual const Ref<IndexBuffer>& GetIndexBuffer() const override { return m_IndexBuffer; }
 	private:
 		uint32_t m_RendererID;
+		uint32_t m_VertexBufferIndex = 0;
 		std::vector<std::shared_ptr<VertexBuffer>> m_VertexBuffers; 
 		std::shared_ptr<IndexBuffer> m_IndexBuffer;
     };
